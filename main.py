daa85a36d0f0d1d8f5f39b90b9b427d3e4620d22
Zachariah Sharma <zachariah.sharma@icloud.com>
2020-09-24 18:03:38 -0400

    adding stuff

---
+++ main.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost", user="root", password="root1234", database="mydemodb")
+mycursor = mydb.cursor()
+sql = "select * from student"
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for x in myresult:
+    print(x)
